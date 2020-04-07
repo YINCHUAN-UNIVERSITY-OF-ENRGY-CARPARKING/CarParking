21effe81fc9270875451595b3294b05939198d3d
Mar Ping <2713241118@qq.com>
2020-04-07 08:55:09 +0800

    V0.0

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-'''
-@Descripttion: Null
-@version: 1.0
-@Author: Mar Ping
-@Date: 2020-04-06 23:56:27
-@LastEditors: Mar Ping
-@LastEditTime: 2020-04-07 00:42:40
-'''
+
 #————————————————————————————————所有模块在这里组装————————————————————————————————————
